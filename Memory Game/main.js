002c198a6573d98049829b54df627e055efe00d2
Zeev Y. Goldberg <zeevygoldberg@gmail.com>
2022-09-30 17:27:42 +0300

    before shabbbbbbas

---
+++ Memory Game/main.js
@@ -0,0 +1,40 @@
+const cardArray = [
+    {
+        name: 'Bread',
+        img: 'Memory Game/images/bread.jpg',
+    },
+    {
+        name: 'Burger',
+        img: 'Memory Game/images/burger.gif',
+    },
+    {
+        name: 'Donut',
+        img: 'Memory Game/images/donut.jpg',
+    },
+    {
+        name: 'Oats',
+        img: 'Memory Game/images/outs.gif',
+    },
+    {
+        name: 'Pancakes',
+        img: 'Memory Game/images/pancakes.jpg',
+    },
+    {
+        name: 'Pizza',
+        img: 'Memory Game/images/pizza.png',
+    },
+]
+cardArray.sort(() => 0.5 - Math.random())//זאת דרך מצוינת לערבב את הסדר של כל אריי
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard(){
+    for (let i=0; i<10; i++){
+        const card = document.createElement('img');
+        card.setAttribute('src', 'Memory Game/images/american.jpg');
+        card.setAttribute('data-id', i);
+        gridDisplay.appendChild(card);
+        console.log(card, i);
+
+    }
+}
+createBoard()
