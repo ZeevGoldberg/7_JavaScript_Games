6d615bc385a3c0e955dffbf1cfe55cc79cc077eb
Zeev Y. Goldberg <zeevygoldberg@gmail.com>
2022-10-02 10:55:39 +0300

    sunday blues

--- Memory Game/main.js
+++ Memory Game/main.js
@@ -28,13 +28,18 @@ cardArray.sort(() => 0.5 - Math.random())//זאת דרך מצוינת לערבב
 const gridDisplay = document.querySelector('#grid')
 
 function createBoard(){
-    for (let i=0; i<10; i++){
+    for (let i=0; i<cardArray.length; i++){
         const card = document.createElement('img');
         card.setAttribute('src', 'Memory Game/images/american.jpg');
         card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard);
         gridDisplay.appendChild(card);
         console.log(card, i);
 
     }
 }
 createBoard()
+
+function flipCard(){
+    const cardId = this.getAttribute('data-id');
+}
